78f4ecc745222528a4d7d5722fa80059920d02cb
Wil Moore III <wil.moore@wilmoore.com>
2013-10-14 01:33:38 -0600

    Update and rename phantom-js.rb to phantomjs.rb

--- Formula/phantomjs.rb
+++ Formula/phantomjs.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class PhantomJs < Formula
+class Phantomjs < Formula
   homepage 'http://phantomjs.org'
   url      'https://phantomjs.googlecode.com/files/phantomjs-1.9.2-macosx.zip'
   sha1     '36357dc95c0676fb4972420ad25455f49a8f3331'
